d59447310b2f818995bc7b04447ba867ab8ac6b2
rebel-jiwonk <jiwon.k@rebellions.ai>
2025-06-10 10:04:18 +0900

    push

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  basePath: '/techblog', // <- Use your repo name
+  assetPrefix: '/techblog',
+  trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
