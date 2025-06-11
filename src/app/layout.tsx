7b522e7a6ee458640d69491307bf4a14beff3595
rebel-jiwonk <jiwon.k@rebellions.ai>
2025-06-11 13:42:01 +0900

    light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko" suppressHydrationWarning>
       <body className="bg-base-50 text-base-800 dark:bg-base-800 dark:text-base-50 font-sans transition-colors duration-200">
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={true}>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={true}>
           <div className="bg-base-50 text-base-800 dark:bg-base-800 dark:text-base-50 min-h-screen flex flex-col">
             <header className="border-b border-base-200 dark:border-base-700 px-6 py-4 flex items-center justify-between">
               <Link href="/" className="text-lg font-bold uppercase">
