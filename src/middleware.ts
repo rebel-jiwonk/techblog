5071786a600ded0ceadf9d92bb11c15a292b471f
rebel-jiwonk <jiwon.k@rebellions.ai>
2025-07-08 15:07:53 +0900

    set password (temporary)

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+const USERNAME = 'rebel';
+const PASSWORD = 'supersecret'; 
+
+export function middleware(req: NextRequest) {
+  const auth = req.headers.get('authorization')
+
+  if (auth) {
+    const [scheme, encoded] = auth.split(' ')
+
+    if (scheme === 'Basic') {
+      const [user, pass] = atob(encoded).split(':')
+
+      if (user === USERNAME && pass === PASSWORD) {
+        return NextResponse.next()
+      }
+    }
+  }
+
+  return new Response('🛑 Authentication required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
+
+export const config = {
+  matcher: ['/', '/((?!_next/static|_next/image|favicon.ico).*)'],
+}
