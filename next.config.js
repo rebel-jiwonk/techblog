77b3b3c1c255ddf83efcd87bdb8e255a5fddc6fe
rebel-jiwonk <jiwon.k@rebellions.ai>
2025-07-14 12:33:05 +0900

    testing config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
     images: {
       remotePatterns: [new URL('https://ryllnxwojdqgnpghelho.supabase.co/storage/v1/object/public/blog-assets/**')],
     },
-    distDir: 'out',
-    trailingSlash: true,
+    // distDir: 'out',
+    // trailingSlash: true,
   };
